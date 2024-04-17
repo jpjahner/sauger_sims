10f1b29f9b49c0c6e635e57c40e8e7d9931cfde4
jpjahner <jpjahner@gmail.com>
2024-04-17 15:06:27 -0600

    no longer delete vcf

--- slim_trials/trial01_slurm.sh
+++ slim_trials/trial01_slurm.sh
@@ -18,9 +18,9 @@ cd /project/evolgen/jjahner/sauger_sims/slim_trials/
 
 module load arcc/1.0 gcc/12.2.0 vcftools/0.1.16
 vcftools --vcf slim_out.vcf --012
-rm slim_out.vcf
+## rm slim_out.vcf
 rm out.012.indv
 rm out.012.pos
-rm out.log
 
+mv slim_out.vcf trial01.vcf
 mv out.012 trial01_genos.txt
