4b54880941810e8ad688c1c6fb8f58989a780315
jjahner <jjahner@uwyo.edu>
2024-08-13 10:58:04 -0600

    switched rounding to two digits to match other figures

--- empirical/unfolded_beta_fit.R
+++ empirical/unfolded_beta_fit.R
@@ -22,8 +22,8 @@ par(new=TRUE)
 dbeta_seq_x <- seq(beta_fit$l, beta_fit$u, length=100)
 dbeta_seq_y <- dBeta.4P(seq(beta_fit$l, beta_fit$u, length=100), beta_fit$l, beta_fit$u, beta_fit$alpha, beta_fit$beta)
 plot(dbeta_seq_x[2:99], dbeta_seq_y[2:99], ylim=c(0,max(dbeta_seq_y[2:99])), type="l", col="red", lwd=2, xlab="", ylab="", axes=FALSE)
-mtext(bquote(alpha==.(round(beta_fit$alpha,3))), cex=1.25, adj=0.02, line=-1.15)
-mtext(bquote(beta==.(round(beta_fit$beta,3))), cex=1.25, adj=0.02, line=-2.4)
+mtext(bquote(alpha==.(round(beta_fit$alpha,2))), cex=1.25, adj=0.02, line=-1.15)
+mtext(bquote(beta==.(round(beta_fit$beta,2))), cex=1.25, adj=0.02, line=-2.4)
 dev.off()
 
 
